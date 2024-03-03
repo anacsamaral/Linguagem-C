00dde619d9aaafa7c9562fc94e1feefbc64c1e31
Ana Sousa <anacarolinesousadoamaral2020@gmail.com>
2024-03-03 14:38:37 -0300

    maior elemento

---
+++ Exercícios de Fixação I/Maior Elemento.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+    int a, b, c;
+    printf("Digite os 3 numeros:\n");
+    scanf("%d%d%d", &a, &b, &c);
+
+    if(a>b)
+        if(a>c)
+            printf("%d é maior", a);
+        else
+            printf("%d é maior", c);
+    else
+    {
+        if(b>c)
+            printf("%d é maior", b);
+        else
+            printf("%d é maior", c);
+    }
+
+    return 0;
+}
+ 
