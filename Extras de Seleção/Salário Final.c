2ef4713ecb85d74f4a9d7d38f97e6fe328fe49a9
Ana Sousa <anacarolinesousadoamaral2020@gmail.com>
2024-03-15 17:23:26 -0300

    salario final corrigido

--- Extras de Seleção/Salário Final.c
+++ Extras de Seleção/Salário Final.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
 int main()
 {
-    int idade, ano_cont, dependentes, ano_atual, anos_casa;
-    float sal_base, sal_final, reajuste1, reajuste2, reajuste3;
-    printf("Digite o salário base:\n");
+    float sal_base, sal_final, reajuste1=0, reajuste2=0, reajuste3=0;
+    int idade, ano_cont, dependentes, ano_atual, ano_casa;
+    printf("Digite o salario base:\n");
     scanf("%f", &sal_base);
-    printf("Digite a idade, o ano de contratação, a idade do casamento, o número de dependentes e o ano atual:\n");
-    scanf("%d%d%d%d%d", &idade, &ano_cont, &anos_casa, &dependentes, &ano_atual);
+    printf("Digite a idade, o ano de contratacao, a idade do casamento, o numero de dependentes e o ano atual:\n");
+    scanf("%d%d%d%d%d", &idade, &ano_cont, &ano_casa, &dependentes, &ano_atual);
 
     if (idade > 25)
-        reajuste1 = 1/100 * sal_base * idade - 25;
-    if (anos_casa > 0)
-        reajuste2 = 3/100 * sal_base * anos_casa;
+        reajuste1 = 0.01 * sal_base * (idade - 25);
+    if (ano_casa > 0)
+        reajuste2 = 0.03 * sal_base * ano_casa;
     if (dependentes > 0)
-        reajuste3 = 5/100 * sal_base * dependentes;
+        reajuste3 = 0.05 * sal_base * dependentes;
     
     sal_final = sal_base + reajuste1 + reajuste2 + reajuste3;
-    printf("Seu salário final é: R$%.2f", &sal_final);
+    printf("Seu salario final e: R$%.2f\n", sal_final);
+
+    return 0;
 }
