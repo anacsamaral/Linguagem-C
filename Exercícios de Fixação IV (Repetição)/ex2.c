6c5ebea83fc488b3eac95326937c27b9b351d0f5
Ana Sousa <anacarolinesousadoamaral2020@gmail.com>
2024-03-21 16:55:25 -0300

    media dos pares

---
+++ Exercícios de Fixação IV (Repetição)/ex2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    int soma = 0, num, contP = 0;
+    float media;
+    printf("Digite os números ou 0 para terminar o programa:\n");
+    scanf("%d", &num);
+    while(num != 0)
+    {
+        if (num % 2 == 0)
+        {
+            contP = contP + 1;
+            soma = soma + num;
+            printf("PARES = %d\n", contP);
+            printf("SOMA = %d\n", soma);
+        }
+        printf("Digite outro numero ou 0 para terminar o programa:\n");
+        scanf(" %d", &num);
+    }
+    media = (float) soma/contP;
+    printf("A media dos numeros pares é: %.1f \n", media);
+    return 0;
+}
