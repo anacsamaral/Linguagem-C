68cc6062a551377091b5ee8e3d725d1e5a5dd98e
Ana Sousa <anacarolinesousadoamaral2020@gmail.com>
2024-03-21 16:12:13 -0300

    soma criado

---
+++ Beecrowd/soma.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+ 
+int main()
+{
+    int A, B, soma, X;
+    printf("digite dois valores:\n");
+    scanf("%d%d", &A, &B);
+    soma = A + B;
+    X = soma;
+    printf("X = %d", X);
+    return 0;
+}
