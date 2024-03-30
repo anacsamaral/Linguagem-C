0fddbcc1800b43e85246ea0684b70e4af3ce5c6e
Ana Sousa <anacarolinesousadoamaral2020@gmail.com>
2024-03-30 15:55:54 -0300

    tempo de jogo

--- Beecrowd/Tempo de Jogo.c
+++ Beecrowd/Tempo de Jogo.c
@@ -1,5 +1,24 @@
 #include <stdio.h>
 int main()
 {
-    
+    int hora_inicial, hora_final, tempo;
+    scanf("%d %d", &hora_inicial, &hora_final);
+    if (hora_inicial > hora_final)
+    {
+        tempo = 24 - hora_inicial;
+        tempo = tempo + hora_final;
+        printf("O JOGO DUROU %d HORA(S)\n", tempo);
+    }
+    else
+    {
+        if (hora_final > hora_inicial)
+        {
+            tempo = hora_final - hora_inicial;
+            printf("O JOGO DUROU %d HORA(S)\n", tempo);
+        }
+        else
+            printf("O JOGO DUROU 24 HORA(S)\n");
+    }
+
+    return 0;
 }
