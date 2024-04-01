9df035b7d4f29e6e6ad0fc05a6479873724abf1c
Ana Sousa <anacarolinesousadoamaral2020@gmail.com>
2024-04-01 00:28:42 -0300

    experiencias

--- Beecrowd/Experiências.c
+++ Beecrowd/Experiências.c
@@ -20,20 +20,19 @@ int main()
                 quantS += quantCobaias;
         }
     }
-        totCobaias = quantC + quantR + quantS;
-        percentC = (float) quantC/totCobaias * 100;
-        percentR = (float) quantR/totCobaias * 100;
-        percentS = (float) quantS/totCobaias * 100;
+    totCobaias = quantC + quantR + quantS;
+    percentC = (float) quantC/totCobaias * 100;
+    percentR = (float) quantR/totCobaias * 100;
+    percentS = (float) quantS/totCobaias * 100;
 
-        printf("Total: %d cobaias\n", totCobaias);
-        printf("Total de coelhos: %d\n", quantC);
-        printf("Total de ratos: %d\n", quantR);
-        printf("Total de sapos: %d\n", quantS);
-        printf("Percentual de coelhos: %.2f%%\n", percentC);
-        printf("Percentual de ratos: %.2f%%\n", percentR);
-        printf("Percentual de sapos: %.2f%%\n", percentS);
-
-    }
+    printf("Total: %d cobaias\n", totCobaias);
+    printf("Total de coelhos: %d\n", quantC);
+    printf("Total de ratos: %d\n", quantR);
+    printf("Total de sapos: %d\n", quantS);
+    printf("Percentual de coelhos: %.2f%%\n", percentC);
+    printf("Percentual de ratos: %.2f%%\n", percentR);
+    printf("Percentual de sapos: %.2f%%\n", percentS);
+    
     return 0;
         
 }
