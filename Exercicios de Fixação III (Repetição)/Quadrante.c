e2c4cccb891dc79aefd8cde414a0336859e90c00
Ana Sousa <anacarolinesousadoamaral2020@gmail.com>
2024-04-29 16:50:17 -0300

    quadrante

--- Exercicios de Fixação III (Repetição)/Quadrante.c
+++ Exercicios de Fixação III (Repetição)/Quadrante.c
@@ -5,9 +5,22 @@ int main()
     printf("Digite o angulo:\n");
     scanf("%d", &ang);
 
-    while (ang >= -360)
+    while (ang >= -360 && ang <= 360)
     {
         if (ang < 0)
             ang = ang * -1;
+
+        if (ang >= 0 && ang <= 90)
+            printf("1 Quadrante\n");
+        else if (ang >= 91 && ang <= 180)
+            printf("2 Quadrante\n");
+        else if (ang >= 181 && ang <= 270)
+            printf("3 Quadrante\n");
+        else
+            printf("4 Quadrante\n");
+
+        printf("Digite outro angulo:\n");
+        scanf("%d", &ang);
     }
+    printf("angulo maior que 360.\n");
 }
