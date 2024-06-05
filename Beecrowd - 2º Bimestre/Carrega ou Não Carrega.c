9748ddd5333879271a1b6f07c2bb43559da1052a
Ana Sousa <anacarolinesousadoamaral2020@gmail.com>
2024-06-05 15:14:30 -0300

    carrega ou nao carrega

--- Beecrowd - 2º Bimestre/Carrega ou Não Carrega.c
+++ Beecrowd - 2º Bimestre/Carrega ou Não Carrega.c
@@ -2,9 +2,10 @@
 #define tf 2
 int main()
 {
-    int vet[tf], n1, n2 = 0;
-    while (scanf ("%d %d", &vet[0], &vet[1]) != EOF)
-        
-        
-    
+    long long int vet[tf], soma;
+    while (scanf ("%lld %lld", &vet[0], &vet[1]) != EOF)
+    {    
+        soma = vet[0]^vet[1];
+        printf("%lld\n", soma);
+    }
 }
