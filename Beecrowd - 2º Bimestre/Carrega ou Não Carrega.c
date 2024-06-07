a7939bcb2a67347cf820dadc77eb539cf493e3a6
Ana Sousa <anacarolinesousadoamaral2020@gmail.com>
2024-06-06 22:08:52 -0300

    carrega ou nao

--- Beecrowd - 2º Bimestre/Carrega ou Não Carrega.c
+++ Beecrowd - 2º Bimestre/Carrega ou Não Carrega.c
@@ -1,11 +1,22 @@
 #include <stdio.h>
-#define tf 2
+#define tf 32
+
 int main()
 {
-    long long int vet[tf], soma;
-    while (scanf ("%lld %lld", &vet[0], &vet[1]) != EOF)
-    {    
-        soma = vet[0]^vet[1];
-        printf("%lld\n", soma);
+    int n1, vet1[tf], i, pos;
+    printf("Digite dois numeros. 0 - SAIR\n");
+    while (scanf ("%d", &n1) != 0)
+    {
+        for (i = 0; i < tf-1; i++)
+        {
+            for (i = 0; i < tf-1 && )
+            if (n1 % 2^(tf-1) == 0)
+                vet1[i] = 0;
+            else
+                vet1[i] = 1;
+        }
+        for (i = 0; i < tf; i++)
+            printf("%d ", vet1[i]);
     }
+    printf("num invalido\n");
 }
