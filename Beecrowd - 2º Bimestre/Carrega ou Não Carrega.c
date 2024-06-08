7585a17f7ef43a02e4c6e2ce35be714b55cc2e6e
Ana Sousa <anacarolinesousadoamaral2020@gmail.com>
2024-06-07 22:08:31 -0300

    update

--- Beecrowd - 2º Bimestre/Carrega ou Não Carrega.c
+++ Beecrowd - 2º Bimestre/Carrega ou Não Carrega.c
@@ -3,20 +3,22 @@
 
 int main()
 {
-    int n1, vet1[tf], i, pos;
-    printf("Digite dois numeros. 0 - SAIR\n");
-    while (scanf ("%d", &n1) != 0)
+    int n1, vet1[tf], i;
+    scanf("%d", &n1);
+    while (n1 != 'EOF' || n1 != 'eof')
     {
-        for (i = 0; i < tf-1; i++)
-        {
-            for (i = 0; i < tf-1 && )
-            if (n1 % 2^(tf-1) == 0)
-                vet1[i] = 0;
-            else
-                vet1[i] = 1;
-        }
         for (i = 0; i < tf; i++)
-            printf("%d ", vet1[i]);
+        {
+        	if (n1 < 2)
+        		vet1[i] = 1;
+        	else
+	        	vet1[i] = n1 % 2;
+	        	n1  = n1 / 2;
+    	}
+        
+        for (i = 0; i < tf; i++)
+        	printf("%d ", &vet1[i]);
+        scanf("%d", &n1);
     }
-    printf("num invalido\n");
+    return 0;
 }
