6c1ae170d43bb632401d68c77e42f7b2c6234055
Bowie <anacarolinesousadoamaral2020@gmail.com>
2024-06-19 19:24:12 -0300

    numero perfeito

--- Exercícios de Fixação IV (Repetição)/Número Perfeito.c
+++ Exercícios de Fixação IV (Repetição)/Número Perfeito.c
@@ -2,22 +2,23 @@
 int main()
 {
     int num, i, soma;
-    printf("Digite o numero (digite um numero negativo para sair):\n");
+    printf("Digite o numero (zero para sair):\n");
     scanf("%d", &num);
-    while (num > 0)  // loop infinito
+    while (num > 0)
     {
         soma = 0;
     
-        for (i = 1; i <= num / 2; i++) { // Verifica se há divisores até num/2
+        for (i = 1; i <= num / 2; i++)  // Verifica se há divisores até num/2
+        {
             if (num % i == 0)
                 soma += i;
-
+        }
         if (soma == num)
             printf("Numero perfeito\n");
         else
             printf("Numero nao perfeito\n");
 
-        printf("Digite o numero (digite um numero negativo para sair):\n");
+        printf("Digite o numero (zero para sair):\n");
         scanf("%d", &num);
     }
     return 0;
--- Exercícios de Fixação V/Número perfeito.c
+++ Exercícios de Fixação V/Número perfeito.c
@@ -2,22 +2,24 @@
 int main()
 {
     int num, i, soma;
-    while (1)  // loop infinito
+    printf("Digite o numero (zero para sair):\n");
+    scanf("%d", &num);
+    while (num > 0)
     {
         soma = 0;
-        printf("Digite o numero (digite um numero negativo para sair):\n");
-        scanf("%d", &num);
-
-        if (num <= 0) // termina a condição
-            break;
     
-        for (i = 1; i <= num / 2; i++) { // Verifica se há divisores até num/2
+        for (i = 1; i <= num / 2; i++)  // Verifica se há divisores até num/2
+        {
             if (num % i == 0)
                 soma += i;
+        }
         if (soma == num)
             printf("Numero perfeito\n");
         else
             printf("Numero nao perfeito\n");
+
+        printf("Digite o numero (zero para sair):\n");
+        scanf("%d", &num);
     }
     return 0;
 }
