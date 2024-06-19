a2ff0036e9cd0408e1f60232724b63b632a58826
Bowie <anacarolinesousadoamaral2020@gmail.com>
2024-06-19 15:36:24 -0300

    somatorio 3

--- Exercícios de Fixação V/teste.c
+++ Exercícios de Fixação V/teste.c
@@ -1,22 +1,24 @@
 #include <stdio.h>
 #include <math.h>
+
 int main()
 {
-    int d, n, expo = 0;
-    long long int potencia, soma = 0;
+    int n, expo;
+    long long int potencia, soma;
     printf("Valor de X:\n");
     scanf("%d", &n);
+
     while (n > 0)
     {
         expo = 0;
         soma = 0;
 
-        for (d = 50; d >= 1; d--)
+        for (int d = 50; d >= 1; d--)
         {
             expo++;
             potencia = pow(n, expo);
             soma += potencia / d;
-            
+
             if (expo == 1)
                 printf("S = %lld/%d + ", potencia, d);
             else if (expo == 50)
@@ -24,10 +26,12 @@ int main()
             else
                 printf("%lld/%d + ", potencia, d);
         }
+
         printf("Soma = %lld\n", soma);
         printf("\n");
         printf("Valor de X:\n");
         scanf("%d", &n);
     }
+
     return 0;
 }
