5deb84d1e5ed2390fafb346f8aec0317a659bd84
Bowie <anacarolinesousadoamaral2020@gmail.com>
2024-08-12 16:41:58 -0300

    remanejamento 2 corrigido

--- Exercícios de Fixação VIII (Vetores)/Remanejamento 2.c
+++ Exercícios de Fixação VIII (Vetores)/Remanejamento 2.c
@@ -2,7 +2,7 @@
 #define TF 8
 int main()
 {
-    int vet[TF], i, j, pos, num, elem, TL = 0;
+    int vet[TF], i, pos, num, elem, TL = 0;
     printf("Digite o %do. numero:\n", TL+1);
     scanf("%d", &num);
     while (TL < TF && num > 0)
@@ -18,16 +18,11 @@ int main()
     printf("Digite o numero e a posicao:\n");
     scanf("%d%d", &elem, &pos);
 
-    j = 0;
-    while (vet[j] > 0 && j < TL)
-        j++;
+    vet[TL] = vet[pos];
+    vet[pos] = elem;
+    TL++;
 
-    for (i = pos; i < TL; i++)
-    {
-        vet[pos+TL] = vet[pos];
-        TL++;
-    }
-    for(i = 0; i < TL; i++)
+    for (i = 0; i < TL; i++)
         printf("%d ", vet[i]);
 
     return 0;
