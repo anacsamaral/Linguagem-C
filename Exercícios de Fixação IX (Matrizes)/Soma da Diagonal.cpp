549324a9ed6551142ed90e342d7ad2faa5d506ad
Bowie <anacarolinesousadoamaral2020@gmail.com>
2025-01-03 19:00:59 -0300

    soma da diagonal

--- Exercícios de Fixação IX (Matrizes)/Soma da Diagonal.cpp
+++ Exercícios de Fixação IX (Matrizes)/Soma da Diagonal.cpp
@@ -1,1 +1,21 @@
 #include <stdio.h>
+#define TF 3
+
+int main()
+{
+    int l, c, somaD = 0, Mat[TF][TF];
+
+    for(l = 0; l < TF; l++)
+        for(c = 0; c < TF; c++)
+        {
+            printf("\nElemento [%d|%d]: ", l, c);
+            scanf("%d", &Mat[l][c]);
+        }
+
+    for(c = 0, l = TF - 1; c < TF && l >= 0; c++, l--)
+        somaD += Mat[l][c];
+    
+    printf("\nA soma dos elementos da diagonal principal eh %d.", somaD);
+
+    return 0;
+}
