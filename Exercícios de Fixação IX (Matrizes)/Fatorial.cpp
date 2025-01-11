22c2a0c3fe20d489c53ebef91bb28b97654efcb9
Bowie <anacarolinesousadoamaral2020@gmail.com>
2025-01-11 19:24:49 -0300

    update fatorial

--- Exercícios de Fixação IX (Matrizes)/Fatorial.cpp
+++ Exercícios de Fixação IX (Matrizes)/Fatorial.cpp
@@ -9,37 +9,45 @@ int main()
 
     int Matriz[linhas][colunas];
 
-    for(l = 0; l < linhas; l++)
-        for(c = 0; c < colunas; c++)
+    // Preenchendo a matriz
+    for (l = 0; l < linhas; l++)
+        for (c = 0; c < colunas; c++) 
         {
             printf("\nElemento [%d][%d]: ", l, c);
             scanf("%d", &Matriz[l][c]);
         }
 
+    // Calculando fatoriais ou zerando elementos negativos
     for (l = 0; l < linhas; l++)
-        for (c = 0; c < colunas; c++)
-            num = Matriz[l][j];
+        for (c = 0; c < colunas; c++) 
+        {
+            num = Matriz[l][c];
             if (num < 0) 
-                Matriz[l][c] = 0;
-            else 
+                Matriz[l][c] = 0; // Substitui números negativos por 0
+            else if (num > 12)
+            { 
+                // Evita overflow: Fatorial de 13 já excede o limite de int
+                printf("\nErro: Fatorial de %d excede o limite de um int.\n", num);
+                Matriz[l][c] = -1; // Usa -1 para indicar um valor inválido
+            } 
+            else
             {
                 fatorial = 1;
-                for (int k = 1; k <= valor; k++) {
+                for (int k = 1; k <= num; k++)
                     fatorial *= k;
-                }
-                matriz[l][j] = fatorial;
+                Matriz[l][c] = fatorial;
             }
         }
 
-    // Imprime a matriz atualizada
+    // Imprimindo a matriz atualizada
     printf("\nMatriz atualizada com os fatoriais:\n");
-    for (int i = 0; i < linhas; i++) {
-        for (int j = 0; j < colunas; j++) {
-            printf("%llu ", (unsigned long long)matriz[i][j]);
-        }
+    for (l = 0; l < linhas; l++) 
+    {
+        for (c = 0; c < colunas; c++)
+            printf("%d ", Matriz[l][c]);
+
         printf("\n");
     }
 
     return 0;
-
 }
