52358c43a4cb4ed04318b55c95a30162429e8982
Bowie <anacarolinesousadoamaral2020@gmail.com>
2025-01-19 10:51:33 -0300

    alterações

--- Exercícios de Fixação IX (Matrizes)/Estoque2.cpp
+++ Exercícios de Fixação IX (Matrizes)/Estoque2.cpp
@@ -1,89 +1,89 @@
 #include <stdio.h>
 #include <string.h>
+#include <limits.h>
 
 #define TFP 10
 #define TFA 5
 
 int main()
 {
-    int l, c, Estoque[TFA][TFP], Total_Armazem, Total_Produto, Custo_Armazem, Armazem_Menor_Estoque, Armazem_Maior_Estoque, Maior_Estoque, Menor_Estoque;
-    float Custo[TFP];
-    char Produto[TFP][20], Produto_Menor_Estoque, Produto_Maior_Estoque;
+    int l, c, Estoque[TFA][TFP], Total_Armazem, Total_Produto, Armazem_Menor_Estoque, Armazem_Maior_Estoque;
+    float Custo[TFP], Custo_Armazem;
+    char Produto[TFP][20], Produto_Menor_Estoque[20], Produto_Maior_Estoque[20];
+    int Maior_Estoque = INT_MIN, Menor_Estoque = INT_MAX;
 
-    for(c = 0; c < TFP; c++)
+    // Entrada de dados
+    for (c = 0; c < TFP; c++)
     {
-        printf("\nDigite o nome do %do. produto: ", c+1); fflush(stdin);
-        scanf("%s", &Produto[c]);
-        printf("\nDigite o custo do %do. produto: ", c+1);
+        printf("\nDigite o nome do %do produto: ", c + 1);
+        scanf("%s", Produto[c]);
+        printf("Digite o custo do %do produto: ", c + 1);
         scanf("%f", &Custo[c]);
 
-        for(l = 0; l < TFA; l++)
+        for (l = 0; l < TFA; l++)
         {
-            printf("\nDigite o estoque do %do. produto no armazem %d: ", c+1, l+1);
+            printf("Digite o estoque do %do produto no armazém %d: ", c + 1, l + 1);
             scanf("%d", &Estoque[l][c]);
         }
     }
 
-    //Total de itens armazenados em cada armazÃ©m;
-    //Total de itens armazenados de cada produto em todos os armazÃ©ns juntos;
-    //Produto e nÃºmero do armazÃ©m que possui o maior nÃºmero de itens estocados;
-    //Produto e nÃºmero do armazÃ©m que possui o menor nÃºmero de itens estocados;
-
-    Menor_Estoque = Maior_Estoque = Estoque[l-1][c-1];
-    for(l = 0; l < TFA; l++)
+    // Cálculo total por armazém
+    for (l = 0; l < TFA; l++)
     {
         Total_Armazem = 0;
-        for(c = 0; c < TFP; c++)
+        for (c = 0; c < TFP; c++)
             Total_Armazem += Estoque[l][c];
-        if(Total_Armazem > Maior_Estoque)
+
+        if (Total_Armazem > Maior_Estoque)
         {
             Maior_Estoque = Total_Armazem;
-            Armazem_Maior_Estoque = l+1;
+            Armazem_Maior_Estoque = l + 1;
         }
-        if(Total_Armazem < Menor_Estoque)
+        if (Total_Armazem < Menor_Estoque)
         {
             Menor_Estoque = Total_Armazem;
-            Armazem_Menor_Estoque = l+1;
+            Armazem_Menor_Estoque = l + 1;
         }
-        printf("Estoque do armazem %d: [%d]", l+1, Total_Armazem);
+        printf("\nEstoque do armazém %d: [%d]", l + 1, Total_Armazem);
     }
-    printf("\nArmazem com maior Estoque: %d", Armazem_Maior_Estoque);
-    printf("\nArmazem com menor Estoque: %d", Armazem_Menor_Estoque);
-    
-    Maior_Estoque = Menor_Estoque = Estoque[l][c];
-    for(c = 0; c < TFP; c++)
+
+    printf("\nArmazém com maior estoque: %d", Armazem_Maior_Estoque);
+    printf("\nArmazém com menor estoque: %d", Armazem_Menor_Estoque);
+
+    // Cálculo total por produto
+    Maior_Estoque = INT_MIN;
+    Menor_Estoque = INT_MAX;
+    for (c = 0; c < TFP; c++)
     {
         Total_Produto = 0;
-        for(l = 0; l < TFP; l++)
+        for (l = 0; l < TFA; l++)
             Total_Produto += Estoque[l][c];
-        if(Total_Produto > Maior_Estoque)
+
+        if (Total_Produto > Maior_Estoque)
         {
             Maior_Estoque = Total_Produto;
             strcpy(Produto_Maior_Estoque, Produto[c]);
         }
-        if(Total_Produto < Menor_Estoque)
+        if (Total_Produto < Menor_Estoque)
         {
             Menor_Estoque = Total_Produto;
             strcpy(Produto_Menor_Estoque, Produto[c]);
-        }    
-        printf("Estoque do produto %s: [%d]", Produto[c], Total_Produto);
+        }
+        printf("\nEstoque do produto %s: [%d]", Produto[c], Total_Produto);
     }
-    printf("\nProduto com maior Estoque: %s", Produto_Maior_Estoque);
-    printf("\nProduto com menor Estoque: %s", Produto_Menor_Estoque);
 
+    printf("\nProduto com maior estoque: %s", Produto_Maior_Estoque);
+    printf("\nProduto com menor estoque: %s", Produto_Menor_Estoque);
 
-    //Custo total de cada armazÃ©m
-    for(l = 0; l < TFA; l++)
+    // Cálculo do custo total por armazém
+    for (l = 0; l < TFA; l++)
     {
         Custo_Armazem = 0;
-        for(c = 0; c < TFP; c++)
+        for (c = 0; c < TFP; c++)
             Custo_Armazem += Custo[c] * Estoque[l][c];
-        printf("Custo do armazem %d: [%d]", l+1, Custo_Armazem);
+        printf("\nCusto do armazém %d: [%.2f]", l + 1, Custo_Armazem);
     }
-
-    //Produto que possui o menor custo
-
-    //Produto que possui o maior custo
 
     return 0;
 }
+
